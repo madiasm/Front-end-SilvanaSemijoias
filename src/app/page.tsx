d4a22c4d2ddfcbe1f32ed9c53b21f5bc875b3eec
ViniciusSotana <vinysotana@gmail.com>
2025-06-07 20:05:08 -0300

    configura o ambiente para desenvolvimento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-   </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
+
+      <h2>
+          Projeto
+      </h2>
+    </div>
   );
 }
